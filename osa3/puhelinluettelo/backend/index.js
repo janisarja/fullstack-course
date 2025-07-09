23596aaca972b749227421abcdcb6d9de16eff41
Jani Sarja <jani.sarja@tuni.fi>
2025-07-09 22:34:06 +0300

    Configurations and remove cors

--- osa3/puhelinluettelo/backend/index.js
+++ osa3/puhelinluettelo/backend/index.js
@@ -27,9 +27,9 @@ let persons = [
 
 const app = express();
 
-app.use(cors());
-
 app.use(express.json());
+
+app.use(express.static('dist'));
 
 morgan.token('body', function (req, res) { return req.method === 'POST' ? JSON.stringify(req.body) : '' })
 
